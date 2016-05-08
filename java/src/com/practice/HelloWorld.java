e1b2421cce6b80cf4dcdf081ed650cb5255a45e5
Tim Timalsina <Tim@unknownf45c89961821.att.net>
2016-05-08 12:24:48 -0700

    HelloWorld Static method

---
+++ java/src/com/practice/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class HelloWorld {
+	
+	public static void welcome() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld.welcome();
+	}
+
+}
